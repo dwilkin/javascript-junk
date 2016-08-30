5aed00781d41dfd50f5704758d25efb5c13ee025
dwilkin <darian@wilkin.online>
2016-08-29 23:46:14 -0700

    Way simpler
    removed tally entirely, moved everything into one for loop basically

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -4,12 +4,11 @@ var questions = [
   ["Jimmy cracked corn.  Do you care?", "NO"],
   ["How much chuck would a wood chuck chuck?", "BANANA"]
 ];
-var answer = "";
+var answer;
 var right = "<h2>You got these questions correct:</h2><ol>";
 var wrong = "</ol><h2>You got these questions incorrect:</h2><ol>";
-var message = "";
+var message;
 var correct = 0;
-var tally = [];
 
 function print(message) {
   document.write(message);
@@ -19,14 +18,6 @@ for (var i = 0; i < questions.length; i++) {
   answer = prompt(questions[i][0]);
   if (answer.toUpperCase() === questions[i][1]){
     correct++;
-    tally.push(0);
-  } else {
-    tally.push(1);
-  }
-}
-
-for (var i = 0; i < questions.length; i++) {
-  if (tally[i] === 0) {
     right += "<li>" + questions[i][0] + "</li>";
   } else {
     wrong += "<li>" + questions[i][0] + "</li>";
